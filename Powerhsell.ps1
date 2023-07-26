1a0297446b580074da85c5c5e4f0b977e300ea9a
kekeli-the-light <kekelidompeh@gmail.com>
2023-07-26 13:43:59 +0000

    Some Powershell today

--- Powerhsell.ps1
+++ Powerhsell.ps1
@@ -1,4 +1,11 @@
 #First Powerhsell 
+#cmdlet
 $Name = Read-Host 'What is Your name'
 
-Write-Host "Welcome to earth, $Name"
+Write-Host "Welcome to earth, $Name " -NoNewline
+Write-Host "It's good to see you learn Powershell!"
+# "May the force be with you" | Out-File forcewithmike.txt
+$House = "Obuasi"
+# Saving content of variable into a file by piping
+$House | Out-File home.txt
+$House.GetType()
